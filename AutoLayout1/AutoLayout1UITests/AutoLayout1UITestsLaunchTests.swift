cf30bf55030f67691ffea5b2b58f9f4a315bf5ea
dikshurwt <147218282+dikshurwt@users.noreply.github.com>
2023-10-06 21:19:44 -0400

    Add files via upload

---
+++ AutoLayout1/AutoLayout1UITests/AutoLayout1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AutoLayout1UITestsLaunchTests.swift
+//  AutoLayout1UITests
+//
+//  Created by user235382 on 10/6/23.
+//
+
+import XCTest
+
+final class AutoLayout1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
